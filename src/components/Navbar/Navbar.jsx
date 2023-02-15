f41de45eb95f44622c15f7789505682334c4287e
subiga028 <subigagunaseelan@gmail.com>
2023-02-15 10:48:31 +0530

    commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     <>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="https://i.ibb.co/kmQBVMP/kongu-logo.png" alt="" /></a>
+        <a class="navbar-brand" href="#"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzH7bPeL_LVAl4U7z8Z-kaeYIRSiYg0nvoNQETe4CVYu8cZRjw-qEGkhiqz9LNY8NcbaY&usqp=CAU" alt="" /></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
@@ -43,10 +43,10 @@ export default function Navbar() {
             <a class="nav-link disabled">Disabled</a>
             </li> */}
         </ul>
-        <form class="d-flex">
+        {/* <form class="d-flex">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
             <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        </form> */}
         </div>
     </div>
     </nav>
